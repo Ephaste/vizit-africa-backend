6f0a911f6c8284d6db7a56728a03cb0e1e3c4e9d
Don-tresor05 <dontresor922@gmail.com>
2026-02-04 17:51:28 +0200

    updated requirements

--- vizitAfricaBackend/settings.py
+++ vizitAfricaBackend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -72,7 +73,8 @@ ROOT_URLCONF = 'vizitAfricaBackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
